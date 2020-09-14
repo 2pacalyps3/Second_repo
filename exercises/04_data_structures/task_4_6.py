aa9e4802cb61bebbf18b1287250107733a73b15f
2pacalyps3 <nurik_umirov@mail.ru>
2020-09-14 17:48:21 +0600

    Task 4.6 is done

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -12,17 +12,26 @@ Outbound Interface    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
-
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
 ospf1 = ospf_route.split()
 ospf1.pop(2)
-print("{ospf:30}".format(ospf = "Prefix") + "{route}".format(route = ospf1[0]))
-print("{ospf_1:30}".format(ospf_1 = "AD/Metric") + "{route}".format(route = ospf1[1]))
-print("{ospf_2:30}".format(ospf_2 = "Next-Hop") + "{route}".format(route = ospf1[2]))
-print("{ospf_3:30}".format(ospf_3 = "Last update") + "{route}".format(route = ospf1[3]))
-print("{ospf_4:30}".format(ospf_4 = "Outbound Interface") + "{route}".format(route = ospf1[4]))
 
+#1 вариант
+ospf2 = """
+Prefix                {}
+AD/Metric             {}
+Next-Hop              {}
+Last update           {}
+Outbound Interface    {} 
+"""
+print(ospf2.format(ospf1[0], ospf1[1], ospf1[2], ospf1[3], ospf1[4]))
 
+#2 вариант
+print("{ospf:25}".format(ospf = "Prefix") + "{route}".format(route = ospf1[0]))
+print("{ospf_1:25}".format(ospf_1 = "AD/Metric") + "{route}".format(route = ospf1[1]))
+print("{ospf_2:25}".format(ospf_2 = "Next-Hop") + "{route}".format(route = ospf1[2]))
+print("{ospf_3:25}".format(ospf_3 = "Last update") + "{route}".format(route = ospf1[3]))
+print("{ospf_4:25}".format(ospf_4 = "Outbound Interface") + "{route}".format(route = ospf1[4]))
 
 
 
