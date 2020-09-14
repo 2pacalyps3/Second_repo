51da8c86c15cfff363c1b8dfb2c5fbe9dd16193b
2pacalyps3 <nurik_umirov@mail.ru>
2020-09-14 10:52:41 +0600

    Task 4.6 is done

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -14,3 +14,18 @@ Outbound Interface    FastEthernet0/0
 """
 
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+
+ospf1 = ospf_route.split()
+ospf1.pop(2)
+print(ospf1)
+ospf2 = ('''
+Prefix                       {}
+AD/Metric                    {}
+Next-Hop                     {}
+Last update                  {}
+Outbound Interface           {}
+''')
+print(ospf2.format(ospf1[0], ospf1[1], ospf1[2], ospf1[3], ospf1[4]))
+
+
+
