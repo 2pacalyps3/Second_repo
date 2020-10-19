a17c766729967b859d93849dba58eaed2f9e8959
2pacalyps3 <nurik_umirov@mail.ru>
2020-10-19 12:32:27 +0600

    task 7.3b is done

--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -11,3 +11,19 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+f = open("CAM_table.txt")
+f1 = f.read().strip().split('\n')
+vlan = input("Enter VLAN: ")
+list_1 = []
+for i in range(len(f1)):
+    a = f1[i].startswith(" ")
+    if a == True:
+        b = f1[i].split()
+        b.pop(2)
+        if b[0].isdigit():
+            b[0] = int(b[0])
+            list_1.append(b)
+for k in range(len(list_1)):
+    if vlan == str(list_1[k][0]):
+        print(list_1[k])
+
