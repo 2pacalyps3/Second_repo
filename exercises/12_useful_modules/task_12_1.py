4d69b65f3e47c4740c68b3e297ce2e821249699b
2pacalyps3 <nurik_umirov@mail.ru>
2020-10-28 18:12:19 +0600

    Task 12.1 is done

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -14,3 +14,20 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+import subprocess
+def ping_ip_addresses(*args):
+    a = []
+    b = []
+    for i in args:
+        result = subprocess.run(["ping", "-c", "1", "-n", i])
+        if result.returncode == 0:
+            a.append(i)
+        else:
+            b.append(i)
+        c = (a, b) 
+    print(c)
+    return c
+    
+ping_ip_addresses("a", "8.8.8.8", "10.1.0.7", "1.1.1")
+
+
